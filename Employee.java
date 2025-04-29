74ab2c97e13ebf64266bfa24a70ef42fb3ecaaf5
MamathaKadamur <mamatavk29@gmail.com>
2025-04-29 08:57:33 +0530

    Save

--- Employee.java
+++ Employee.java
@@ -5,12 +5,14 @@ public class Employee {
     String name;
     int id;
     double salary;
+    String address;
 
     //Constructor
-    public Employee(String name, int id, double salary){
+    public Employee(String name, int id, double salary,String address){
         this.name=name;
         this.id=id;
         this.salary=salary;
+        this.address=address;
     }
 
     //Method to display Employee details
@@ -18,12 +20,14 @@ public class Employee {
         System.out.println("Employee Id:"+ id);
         System.out.println("Employee Name:"+ name);
         System.out.println("Salary Id:"+ salary);
+        System.out.println("address Id:"+ address);
+
     }
 
     //Main Method
     public static void main(String[] args) {
         //
-        Employee emp1=new Employee("Mamatha", 306, 50000.0);
+        Employee emp1=new Employee("Mamatha", 306, 50000.0,"Hubli");
         emp1.displayInfo();
     }
 }
